5b3c6e96eb06946df049229e443e295907b1f111
ravivaghela07908 <vaghelarv6-00@gmail.com>
2022-12-13 12:14:54 +0530

    changes in table

--- Queries.sql
+++ Queries.sql
@@ -6,21 +6,21 @@ CREATE DATABASE employee;
 -- Create hobby table in employee database
 -----------------------------------------------------------------------------
 CREATE TABLE hobby ( 
-    `id` INT(100) NOT NULL, 
-    `name` VARCHAR(200) NOT NULL,
-    PRIMARY KEY (`id`)
+    `id` INT(10) NOT NULL, 
+    `name` VARCHAR(100) NOT NULL, 
+    PRIMARY KEY (`id`) 
 );
 
 -----------------------------------------------------------------------------
 -- Create employee table in employee database
 -----------------------------------------------------------------------------
 CREATE TABLE employee ( 
-    `id` INT(100) NOT NULL, 
-    `first_name` VARCHAR(200) NOT NULL, 
-    `last_name` VARCHAR(200) NOT NULL, 
-    `age` INT(3) NOT NULL, 
-    `mobile_number` INT(10) NOT NULL, 
-    `address` VARCHAR(500) NOT NULL, 
+    `id` INT(10) NOT NULL, 
+    `first_name` VARCHAR(50) NOT NULL, 
+    `last_name` VARCHAR(50) NOT NULL, 
+    `age` TINYINT NOT NULL, 
+    `mobile_number` VARCHAR(17) NOT NULL, 
+    `address` VARCHAR(200) NOT NULL, 
     PRIMARY KEY (`id`) 
 ); 
 
@@ -28,24 +28,24 @@ CREATE TABLE employee (
 -- Create employee_salary table in employee database
 -------------------------------------------------------------------------------
 CREATE TABLE employee_salary ( 
-    `id` INT(100) NOT NULL, 
-    `salary` INT(100) NOT NULL, 
+    `id` INT(10) NOT NULL, 
+    `salary` DECIMAL(10,5) NOT NULL, 
     `date` DATE NOT NULL, 
-    `emp_id` INT(100) NOT NULL, 
+    `fk_employee_id` INT(10) NOT NULL, 
     PRIMARY KEY (`id`) , 
-    FOREIGN KEY (`emp_id`) REFERENCES `employee`(`id`) 
-); 
+    FOREIGN KEY (`fk_employee_id`) REFERENCES `employee`(`id`) 
+);
 
 ------------------------------------------------------------------------------
 -- Create employee_hobby table in employee database
 ------------------------------------------------------------------------------
 CREATE TABLE employee_hobby ( 
-    `id` INT(100) NOT NULL, 
-    `emp_id` INT(100) NOT NULL, 
-    `hobby_id` INT(100) NOT NULL, 
+    `id` INT(10) NOT NULL, 
+    `fk_employee_id` INT(10) NOT NULL, 
+    `fk_hobby_id` INT(10) NOT NULL, 
     PRIMARY KEY (`id`) , 
-    FOREIGN KEY (`emp_id`) REFERENCES `employee`(`id`), 
-    FOREIGN KEY (`hobby_id`) REFERENCES `hobby`(`id`) 
+    FOREIGN KEY (`fk_employee_id`) REFERENCES `employee`(`id`), 
+    FOREIGN KEY (`fk_hobby_id`) REFERENCES `hobby`(`id`) 
 ); 
 
 ------------------------------------------------------------------------------
